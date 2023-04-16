d62a7a64503e55014dba551af5b1ec7f91da5c91
Ben Luo <ben.luo02@gmail.com>
2023-04-16 19:26:09 -0400

    Added Intro Dialogue Scene
    Can walk around and look around!

--- Assets/2D Level Demo/Level Scripts/PlayerMovement.cs
+++ Assets/2D Level Demo/Level Scripts/PlayerMovement.cs
@@ -8,17 +8,31 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 3.5f; // adjust this to change the speed of movement
     private Animator anim;
 
+    private SpriteRenderer sprite;
+
     private float dirX = 0f;
+
+    //2D Point and Click Attempt!
+    //private Vector2 followSpot;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        //followSpot = transform.position; // follows the mouse click
     }
 
     // Update is called once per frame
     void Update()
     {
+        //var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //if(Input.GetMouseButtonDown(0))
+        //{
+            //followSpot = new Vector2(mousePosition.x, transform.position.y);
+        //}
+        //transform.position = Vector2.MoveTowards(transform.position, followSpot, Time.deltaTime * speed);
         dirX = Input.GetAxisRaw("Horizontal"); // get horizontal input from arrow keys
         Vector3 position = transform.position; // get current position of the sprite
 
@@ -36,10 +50,12 @@ public class PlayerMovement : MonoBehaviour
         if (dirX > 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = true;
         }
         else
         {
