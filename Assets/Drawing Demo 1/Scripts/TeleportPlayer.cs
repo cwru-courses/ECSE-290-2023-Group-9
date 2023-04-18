98a0b6ec3e83a98dce0544042f1cf54c032e4220
PCahill44 <ptc34@case.edu>
2023-04-18 18:55:30 -0400

    teleportation between doors

---
+++ Assets/Drawing Demo 1/Scripts/TeleportPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    public GameObject professorMan;
+
+    //public GameObject transportStart;
+    public GameObject transportDestination;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+            if (hit.collider.transform == this.transform)
+            {
+                StartCoroutine(StairsMoment());
+            }
+
+        }
+    }
+
+    IEnumerator StairsMoment()
+    {
+        // professorman animation start
+        // wait for however long animation
+        // Black Screen?
+
+        professorMan.gameObject.transform.position = transportDestination.gameObject.transform.position;
+        //Black Screen Disappear
+        yield return null;
+    }
+}
