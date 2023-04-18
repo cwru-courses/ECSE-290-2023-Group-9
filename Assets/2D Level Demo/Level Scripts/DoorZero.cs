e130ff9711e6f0bcfb82ddff7c46249084d46ae7
Zoe Goldberg <zgoldberg22@gmail.com>
2023-04-18 12:26:05 -0400

    added objects in both levels, animations, and professor walking cycle

--- Assets/2D Level Demo/Level Scripts/DoorZero.cs
+++ Assets/2D Level Demo/Level Scripts/DoorZero.cs
@@ -9,11 +9,16 @@ public class DoorZero : MonoBehaviour
 
     public DialogManager dialogManager;
     public string[] DoorZerolines;
+    // private Animator anim;
+    public Sprite sprite;  
+    public Sprite highlightSprite; 
 
     // Start is called before the first frame update
     void Start()
     {
         canClick = false;
+        // highlightSprite = GetComponent<SpriteRenderer>();
+        // sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -23,12 +28,23 @@ public class DoorZero : MonoBehaviour
     }
     private void OnMouseEnter()
     {
-        if (canClick) { Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto); } // change the mouse icon to the specified texture when the mouse enters the object
+        transform.GetComponent<SpriteRenderer>().sprite = highlightSprite;
+        if (canClick) { 
+            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto); 
+             
+            
+        } // change the mouse icon to the specified texture when the mouse enters the object
     }
 
     private void OnMouseExit()
     {
-        if (canClick){ Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);}// reset the mouse icon to the default when the mouse exits the object
+        if (canClick){ 
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+            
+            
+        }// reset the mouse icon to the default when the mouse exits the object
+        transform.GetComponent<SpriteRenderer>().sprite = sprite; 
+        
     }
 
     private void OnMouseDown()
