1efa6c1e42aa87e3204ffd58c8e022b2a9addd22
Ben Luo <ben.luo02@gmail.com>
2023-04-28 03:03:50 -0400

    BUG: How To Reset Game For Replayability? added Final Credits

--- Assets/End.cs
+++ Assets/End.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
+    IEnumerator LoadSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(12.5f);
+        SceneManager.LoadScene("Credits");
     }
 
     // Update is called once per frame
---
+++ Assets/thankyou.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class thankyou : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent1;
+    public TextMeshProUGUI textComponent2;
+    public TextMeshProUGUI textComponent3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ActivateTextAfterDelay1());
+        StartCoroutine(ActivateTextAfterDelay2());
+        StartCoroutine(ActivateTextAfterDelay3());
+        StartCoroutine(end1());
+        StartCoroutine(end2());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Quit Pressed!");
+        }
+    }
+
+    IEnumerator ActivateTextAfterDelay1()
+    {
+        yield return new WaitForSeconds(3.5f);
+        textComponent1.gameObject.SetActive(true);
+    }
+
+    IEnumerator ActivateTextAfterDelay2()
+    {
+        yield return new WaitForSeconds(6f);
+        textComponent2.gameObject.SetActive(true);
+    }
+
+    IEnumerator ActivateTextAfterDelay3()
+    {
+        yield return new WaitForSeconds(10.0f);
+        textComponent1.gameObject.SetActive(false);
+        textComponent2.gameObject.SetActive(false);
+        textComponent3.gameObject.SetActive(true);
+    }
+
+    IEnumerator end1()
+    {
+        yield return new WaitForSeconds(17.0f);
+        textComponent1.gameObject.SetActive(false);
+        textComponent2.gameObject.SetActive(false);
+        textComponent3.gameObject.SetActive(false);
+    }
+
+    IEnumerator end2()
+    {
+        yield return new WaitForSeconds(19.0f);
+        SceneManager.LoadScene("Start");
+        //Application.LoadLevel(0);
+    }
+}
+
+
+
